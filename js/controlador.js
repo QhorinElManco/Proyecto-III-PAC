dfe101db93c2ec56c8631724b7c70252ee810c48
QhorinElManco <43563235+QhorinElManco@users.noreply.github.com>
2018-12-11 02:01:29 -0600

    commit

--- js/controlador.js
+++ js/controlador.js
@@ -1,12 +1,39 @@
-$(document).ready(function() {
-    $("#desplegar").css('display','none');	
-    
-    $("p").mouseenter(function() {
+$(document).ready(function () {
+    $("#desplegar").css('display', 'none');
+
+    $("p").mouseenter(function () {
         $("#desplegar").show('slow');
     });
-                    
-    $("p").mouseleave(function() {
+
+    $("p").mouseleave(function () {
         $("#desplegar").hide('slow');
 
     });
 });
+
+$("#btn-login").click(function () {
+    $.ajax({
+        url: "ajax/login.php",
+        data: "&usuario=" + $("#Usuario").val() + "&password=" + $("#password").val(),
+        dataType: JSON,
+        method: "POST",
+        success: function (respuesta) {
+            console.log(respuesta);
+            if (respuesta.estatus == 1) {
+                if (respuesta.tipoUsuario == "estudiante")
+                    window.location.href = "estudiante.php";
+                else if (respuesta.tipoUsuario == "docente")
+                    window.location.href = "docente.php";
+                else if (respuesta.tipoUsuario == "docente")
+                    window.location.href = "coordinador-asignatura.php";
+                else if (respuesta.tipoUsuario == "docente")
+                    window.location.href = "coordinador-carrera.php";
+                else if (respuesta.tipoUsuario == "docente")
+                    window.location.href = "jefes-departamento.php";
+            }
+            else
+                $("#alert").html('<span style="color:red;">Usuario y/o contraseña incorrecta</span>');
+            
+    }
+    })
+}
