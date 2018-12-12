c68219ac15af96d9b5a378f4eac3fef74da1b381
QhorinElManco <43563235+QhorinElManco@users.noreply.github.com>
2018-12-12 01:13:38 -0600

    commit

--- ajax/login.php
+++ ajax/login.php
@@ -1,27 +1,110 @@
 <?php
     session_start(); //Tiene que ser la primera funcion.
-    //if(tipoUsuario==estudiante){
-        $archivo = fopen("../data/usuarioEstudiante.json","r");
+    if($_POST["tipoUsuario"]==1){
+        $archivo = fopen("../data/usuariosEstudiante.json","r");
         while(($linea=fgets($archivo))){
             $registro = json_decode($linea,true);
             if (
-                $_POST["usuario"]==$registro["usuario"] && 
+                $_POST["cuenta"]==$registro["cuenta"] && 
                 $_POST["password"]==$registro["password"]
             ){
                 $registro["estatus"] = "1"; //Acceso exitoso
                 $registro["mensaje"] = "Acceso autorizado";
-                $_SESSION["usuario"] = $_POST["usuario"];
+                $_SESSION["cuenta"] = $_POST["cuenta"];
                 $_SESSION["tipoUsuario"] = $registro["tipoUsuario"];
+
                 echo json_encode($registro);
                 exit;
             }
         }
-    //}
+        noFound();
+    }
+    elseif($_POST["tipoUsuario"]==2){
+        $archivo = fopen("../data/usuariosDocente.json","r");
+        while(($linea=fgets($archivo))){
+            $registro = json_decode($linea,true);
+            if (
+                $_POST["cuenta"]==$registro["cuenta"] && 
+                $_POST["password"]==$registro["password"]
+            ){
+                $registro["estatus"] = "1"; //Acceso exitoso
+                $registro["mensaje"] = "Acceso autorizado";
+                $_SESSION["cuenta"] = $_POST["cuenta"];
+                $_SESSION["tipoUsuario"] = $registro["tipoUsuario"];
+
+                echo json_encode($registro);
+                exit;
+            }
+        }
+        noFound();
+    }
+    elseif($_POST["tipoUsuario"]==3){
+        $archivo = fopen("../data/coordinadorAsignatura.json","r");
+        while(($linea=fgets($archivo))){
+            $registro = json_decode($linea,true);
+            if (
+                $_POST["cuenta"]==$registro["cuenta"] && 
+                $_POST["password"]==$registro["password"]
+            ){
+                $registro["estatus"] = "1"; //Acceso exitoso
+                $registro["mensaje"] = "Acceso autorizado";
+                $_SESSION["cuenta"] = $_POST["cuenta"];
+                $_SESSION["tipoUsuario"] = $registro["tipoUsuario"];
+
+                echo json_encode($registro);
+                exit;
+            }
+        }
+        noFound();
+    }
+    elseif($_POST["tipoUsuario"]==4){
+        $archivo = fopen("../data/coordinadorCarrera.json.json","r");
+        while(($linea=fgets($archivo))){
+            $registro = json_decode($linea,true);
+            if (
+                $_POST["cuenta"]==$registro["cuenta"] && 
+                $_POST["password"]==$registro["password"]
+            ){
+                $registro["estatus"] = "1"; //Acceso exitoso
+                $registro["mensaje"] = "Acceso autorizado";
+                $_SESSION["cuenta"] = $_POST["cuenta"];
+                $_SESSION["tipoUsuario"] = $registro["tipoUsuario"];
+
+                echo json_encode($registro);
+                exit;
+            }
+        }
+        noFound();
+    }
+    elseif($_POST["tipoUsuario"]==5){
+        $archivo = fopen("../data/jefeDepartamento.json","r");
+        while(($linea=fgets($archivo))){
+            $registro = json_decode($linea,true);
+            if (
+                $_POST["cuenta"]==$registro["cuenta"] && 
+                $_POST["password"]==$registro["password"]
+            ){
+                $registro["estatus"] = "1"; //Acceso exitoso
+                $registro["mensaje"] = "Acceso autorizado";
+                $_SESSION["cuenta"] = $_POST["cuenta"];
+                $_SESSION["tipoUsuario"] = $registro["tipoUsuario"];
+
+                echo json_encode($registro);
+                exit;
+            }
+        }
+        noFound();
+    }
     //No encontro el registro
-   // else{
+    else{
+        noFound();
+    }
+
+
+    function noFound(){
         $registro = array();
         $registro["estatus"] = "0"; //Acceso no autorizado
         $registro["mensaje"] = "Acceso no autorizado";
         echo json_encode($registro);
-    //}
+    }
 ?>
